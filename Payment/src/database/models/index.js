4c2f459cd6fd5674d2b261fb79dbed914b7ece66
Thareendra2000 <thareendra2000@gmail.com>
2024-05-06 21:31:41 +0530

    Revert "Payment Initial commit"
    This reverts commit 5f0aca3087b9c98132cb7468c517e99a002bc98b.

--- Payment/src/database/models/index.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-    ProductModel: require('./Product'),
-    CourseModel: require('./Course'),
-    RatingAndReviewModel: require('./RatingAndReview'),
-    VideosModel: require('./videos'),
-}
