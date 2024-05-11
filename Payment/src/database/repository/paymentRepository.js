e114ab990b946ef56a4463e3453a51d339d07001
Thareendra2000 <thareendra2000@gmail.com>
2024-05-11 16:44:17 +0530

    Added payment repository

---
+++ Payment/src/database/repository/paymentRepository.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const { PaymentModel} = require('../models');
+
+class PaymentRepository {
+
+    async CreatePayment({ userId, amount, currency, status, items, receipt }) {
+
+        const payment = new PaymentModel({
+            userId, amount, currency, status, items, receipt
+        })
+
+        const paymentResult = await payment.save();
+        return paymentResult;
+    }
+
+    async FindPaymentById(id) {
+
+        const payment = await PaymentModel.findById(id);
+        return payment;
+    }
+
+    async FindPaymentByUserId(userId) {
+
+        const payment = await PaymentModel.find({ userId:userId });
+        return payment;
+    }
+
+    async FindPaymentByCourseId(courseId) {
+
+        const payment = await PaymentModel.find({ 'items.course.courseId': courseId });
+        return payment;
+    }
+
+    async FindpaymentByUserIdAndCourseId(userId, courseId) {
+
+        const payment = await PaymentModel.find({ userId: userId, 'items.course.courseId': courseId });
+        return payment;
+    }
+
+    async getAllPayments(){
+        return await PaymentModel.find();
+    }
+
+}
+
+module.exports = PaymentRepository;
