c44feffa038174c715f1cdfa7a8a73a619934f9b
Cooray NTL <tl685998@gmail.com>
2024-05-13 18:03:42 +0530

    update gateway

--- gateway/index.js
+++ gateway/index.js
@@ -7,9 +7,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+ 
 app.use('/user', proxy('http://localhost:8005'));
-app.use('/course', proxy('http://localhost:8002'));
-app.use('/enrollment', proxy('http://localhost:8003'));
+app.use('/course', proxy('http://localhost:8006'));
+app.use('/enrollment', proxy('http://localhost:8007'));
+console.log('listening on port enrollment');
+
+app.listen(8009, () => {
+  console.log('Server running on port 8009');
+});
+
+
+
 console.log('listening on port enrollment');
 
 app.listen(8009, () => {
