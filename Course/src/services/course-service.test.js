69d45b9e1a1aed33787bb87b9bf21ae6a6e01755
Cooray NTL <tl685998@gmail.com>
2024-05-14 18:59:41 +0530

    feat-test cases update

---
+++ Course/src/services/course-service.test.js
@@ -0,0 +1,10 @@
+// which service it is
+describe("CourseService", () => {
+  // Which function
+  describe("CreateCourse", () => {
+    // Which Scenario we are testing
+    test("validate user inputs", () => {});
+
+    test("Validate response", async () => {});
+  });
+});
